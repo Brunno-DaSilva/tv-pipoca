a4f48caa83e766a1b6bb47cb2403de0277b5dd55
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-07-09 16:18:51 -0500

    Created hook for fecthing API data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-// API
-import API from "../API";
 // Configure
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
 
 // Hooks
+import { useHomeFetch } from "../Hooks/useHomeFetch";
 
 // Default Image
 import DefaultImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-      console.log(movies);
-    } catch (error) {
-      setError(true);
-    }
-  };
-
-  //Initial Render
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
+  const { state, loading, error } = useHomeFetch();
 
   return (
     <div>
